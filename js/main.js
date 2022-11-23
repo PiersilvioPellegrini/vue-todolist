8d4661659cfdd9756fa60a15718f9f789b0e2501
PiersilvioPellegrini <pier.pellegrini.23@gmail.com>
2022-11-23 18:52:37 +0100

    COMPLETED + BONUS1

--- js/main.js
+++ js/main.js
@@ -1,11 +1,45 @@
 const { createApp } = Vue;
 
-createApp({
+ const app = createApp({
+  data() {
+    return {
+      taskList: [
+        {
+          text: "task1",
+          completed: true,
+        },
+        {
+          text: "task2",
+          completed: true,
+        },
+        {
+          text: "task3",
+          completed: true,
+        },
+      ],
+      newTaskData: {
+        text: "",
+        completed: "",
+      },
+    };
+       
+    },
+     methods: {
+              addTask() {
+                this.taskList.push({
+                  text: this.newTaskData.text,
+                  completed: false,
+                });
+              },
     
-    data(){
-        return{
-            
-           
-        }
-    }
-}).mount('#app');
+              deleteItem(i) {
+                const confirmed = confirm(
+                  "Sei sicuro di voler eliminare l'elemento?"
+                );
+                if (confirmed) {
+                  this.taskList.splice(i, 1);
+                }
+              },
+            },
+  
+}).mount("#app");
